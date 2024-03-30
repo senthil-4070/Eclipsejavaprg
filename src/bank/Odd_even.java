9ce1debbd65d0c13fbb467cc52f4ff6a8f9ea0ba
senthil-4070 <sktamilan4070>
2024-03-30 08:58:46 +0530

    odd even prg

---
+++ src/bank/Odd_even.java
@@ -0,0 +1,18 @@
+package bank;
+
+public class Odd_even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int a=10;
+	int b=25;
+	if(a%2==0)
+	System.out.println("even");
+	else
+		System.out.println("Odd");
+	
+	
+	}
+
+}
